aa317a74002c1dab7b572a880f856f440e69a8a7
GabiMinecraft02 <gabrielmeresse09@gmail.com>
2026-01-25 20:18:24 +0100

    Refactor user management structure and functions
    Refactor user management to use an object for users and an IP mapping. Remove unnecessary functions and streamline user addition and removal.

--- server/users.js
+++ server/users.js
@@ -1,32 +1,26 @@
-const users = []; // { socketId, ip, pseudo, inVoice, isMuted }
-
-function getPseudoByIp(ip) {
-  const u = users.find(u => u.ip === ip);
-  return u ? u.pseudo : "Invité";
-}
+const users = {};
+const ipMap = {};
 
 function addUser(socketId, pseudo, ip) {
-  const existing = users.find(u => u.ip === ip);
-  if (existing) {
-    existing.socketId = socketId;
-    existing.pseudo = pseudo || existing.pseudo; // garde pseudo existant si pas changé
-  } else {
-    users.push({ socketId, ip, pseudo: pseudo || "Invité", inVoice: false, isMuted: false });
-  }
-}
-
-function updateUserPseudo(socketId, pseudo) {
-  const u = users.find(u => u.socketId === socketId);
-  if (u) u.pseudo = pseudo;
+  ipMap[ip] = pseudo;
+  users[socketId] = { socketId, pseudo };
 }
 
 function removeUser(socketId) {
-  const idx = users.findIndex(u => u.socketId === socketId);
-  if (idx !== -1) users.splice(idx, 1);
+  delete users[socketId];
 }
 
 function getUsers() {
-  return users;
+  return Object.values(users);
 }
 
-module.exports = { addUser, removeUser, updateUserPseudo, getUsers, getPseudoByIp };
+function getPseudoByIp(ip, config) {
+  return ipMap[ip] || config.ipPseudos[ip] || "";
+}
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUsers,
+  getPseudoByIp
+};
