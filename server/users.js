ec3f1aa3478785a38e2b5f212056b7cf40fd38c1
GabiMinecraft02 <gabrielmeresse09@gmail.com>
2026-01-25 18:59:05 +0100

    Refactor users management to use array structure

--- server/users.js
+++ server/users.js
@@ -1,15 +1,32 @@
-const users = {};
+const users = []; // { socketId, ip, pseudo, inVoice, isMuted }
+
+function getPseudoByIp(ip) {
+  const u = users.find(u => u.ip === ip);
+  return u ? u.pseudo : "Invité";
+}
 
 function addUser(socketId, pseudo, ip) {
-  users[socketId] = { pseudo, ip };
+  const existing = users.find(u => u.ip === ip);
+  if (existing) {
+    existing.socketId = socketId;
+    existing.pseudo = pseudo || existing.pseudo; // garde pseudo existant si pas changé
+  } else {
+    users.push({ socketId, ip, pseudo: pseudo || "Invité", inVoice: false, isMuted: false });
+  }
+}
+
+function updateUserPseudo(socketId, pseudo) {
+  const u = users.find(u => u.socketId === socketId);
+  if (u) u.pseudo = pseudo;
 }
 
 function removeUser(socketId) {
-  delete users[socketId];
+  const idx = users.findIndex(u => u.socketId === socketId);
+  if (idx !== -1) users.splice(idx, 1);
 }
 
 function getUsers() {
-  return Object.values(users);
+  return users;
 }
 
-module.exports = { addUser, removeUser, getUsers };
+module.exports = { addUser, removeUser, updateUserPseudo, getUsers, getPseudoByIp };
