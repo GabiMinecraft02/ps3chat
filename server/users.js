c50dc00d6750b66f68334397c6be2a6ad6fd6e55
GabiMinecraft02 <gabrielmeresse09@gmail.com>
2026-01-28 13:00:08 +0100

    Refactor user management to include voice and mute states
    Enhanced user object to include voice and mute status.

--- server/users.js
+++ server/users.js
@@ -1,16 +1,50 @@
 const users = [];
 
+/*
+user = {
+  socketId,
+  pseudo,
+  ip,
+  inVoice,
+  isMuted
+}
+*/
+
 module.exports = {
-  addUser(id, pseudo, ip) {
-    users.push({ socketId: id, pseudo, ip });
+  addUser(socketId, pseudo, ip) {
+    users.push({
+      socketId,
+      pseudo,
+      ip,
+      inVoice: false,
+      isMuted: false
+    });
   },
 
-  removeUser(id) {
-    const index = users.findIndex(u => u.socketId === id);
+  removeUser(socketId) {
+    const index = users.findIndex(u => u.socketId === socketId);
     if (index !== -1) users.splice(index, 1);
   },
 
+  setVoice(socketId, inVoice) {
+    const user = users.find(u => u.socketId === socketId);
+    if (user) user.inVoice = inVoice;
+  },
+
+  setMuted(socketId, isMuted) {
+    const user = users.find(u => u.socketId === socketId);
+    if (user) user.isMuted = isMuted;
+  },
+
   getUsers() {
-    return users;
+    return users.map(u => ({
+      pseudo: u.pseudo,
+      inVoice: u.inVoice,
+      isMuted: u.isMuted
+    }));
+  },
+
+  getUser(socketId) {
+    return users.find(u => u.socketId === socketId);
   }
 };
