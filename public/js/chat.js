e30dd0331869b6fb3f16a3020d002a76dd0577b2
GabiMinecraft02 <gabrielmeresse09@gmail.com>
2026-01-28 17:05:51 +0100

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,6 @@
-const socket = io();
+const socket = io("/", {
+  transports: ["websocket"]
+});
 
 const messagesDiv = document.getElementById("messages");
 const messageInput = document.getElementById("msg");
