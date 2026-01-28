1bdd157c80dc519289413d161301d6c5dd668a7a
GabiMinecraft02 <gabrielmeresse09@gmail.com>
2026-01-28 16:41:20 +0100

    Update users.js

--- server/users.js
+++ server/users.js
@@ -1,50 +1,19 @@
-const users = [];
+const users = {};
 
-/*
-user = {
-  socketId,
-  pseudo,
-  ip,
-  inVoice,
-  isMuted
+function addUser(socketId, pseudo, ip) {
+  users[socketId] = { pseudo, ip };
 }
-*/
+
+function removeUser(socketId) {
+  delete users[socketId];
+}
+
+function getUsers() {
+  return Object.values(users);
+}
 
 module.exports = {
-  addUser(socketId, pseudo, ip) {
-    users.push({
-      socketId,
-      pseudo,
-      ip,
-      inVoice: false,
-      isMuted: false
-    });
-  },
-
-  removeUser(socketId) {
-    const index = users.findIndex(u => u.socketId === socketId);
-    if (index !== -1) users.splice(index, 1);
-  },
-
-  setVoice(socketId, inVoice) {
-    const user = users.find(u => u.socketId === socketId);
-    if (user) user.inVoice = inVoice;
-  },
-
-  setMuted(socketId, isMuted) {
-    const user = users.find(u => u.socketId === socketId);
-    if (user) user.isMuted = isMuted;
-  },
-
-  getUsers() {
-    return users.map(u => ({
-      pseudo: u.pseudo,
-      inVoice: u.inVoice,
-      isMuted: u.isMuted
-    }));
-  },
-
-  getUser(socketId) {
-    return users.find(u => u.socketId === socketId);
-  }
+  addUser,
+  removeUser,
+  getUsers
 };
